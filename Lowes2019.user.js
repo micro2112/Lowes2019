4b95fe0ae889cb344a93c2a69093a69fb8717670
micro2112 <micro2112@users.noreply.github.com>
2019-10-18 12:05:32 -0400

    Updated Paypal Links

--- Lowes2019.user.js
+++ Lowes2019.user.js
@@ -21,7 +21,7 @@
 // @grant       GM_setValue
 // @grant       GM_registerMenuCommand
 // @noframes
-// @version     3.2.2
+// @version     3.2.3
 // @run-at document-end
 // @contributionURL https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=FDW4NZ6PRMDMJ&lc=US&item_name=Lowes%20Price%20Checker&item_number=LPC&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted
 // @contributionAmount $5.00
@@ -41,6 +41,7 @@
 //   3.0 Updated to work on non-mobile site.  Added store picking functionality
 //   3.1 Fixes for Lowes website change
 //   3.2 2019-1004 Updates for website change & github autoupdate
+//      .3 Paypal Links were broken
 
 
 // get our own version of jquery.   The first line should be enough, but Android tampermonkey needed something more explicit....
@@ -340,11 +341,11 @@ var states = [
            <div id='getStDiv' style='margin-left: 40%;' ><button type='button' id='getStores' class='btn btn-green'>Find Stores</button></div></div></DIV>");
     $('#getStores').click(function () { findStores(); });
 
-    $('#donate1Btn').click(function () {
+    $('#donateBtn1').click(function () {
          var win = window.open("https://www.paypal.me/micro2112?locale.x=en_US", '_blank');
          win.focus();
          });
-    $('#donate2Btn').click(function () {
+    $('#donateBtn2').click(function () {
          var win = window.open("https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=FDW4NZ6PRMDMJ&lc=US&item_name=Lowes%20Price%20Checker&item_number=LPC&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted", '_blank');
          win.focus();
          });
